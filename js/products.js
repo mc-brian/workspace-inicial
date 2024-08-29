46b8b760abaa1080c10fe473b37868c4d7fd59d3
Brian <briancamilaa@gmail.com>
2024-08-29 08:50:08 -0300

    Actualización .js y .html de products

--- js/products.js
+++ js/products.js
@@ -7,15 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
             
             data.products.forEach(product => {
                 let productHTML = `
-                    <div class="col-12 col-md-6 col-lg-4 mb-4">
-                        <div class="card h-100">
-                            <img src="${product.image}" class="card-img-top" alt="${product.name}">
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title">${product.name}</h5>
-                                <p class="card-text flex-grow-1">${product.description}</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <span class="text-muted">${product.currency} ${product.cost}</span>
-                                    <small class="text-muted">Vendidos: ${product.soldCount}</small>
+                    <div class="col-12 col-md-6 col-lg-12 mb-4">
+                        <div class="list-group-item list-group-item-action">
+                            <div class="row">
+                                <div class="col-12 col-lg-3">
+                                    <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+                                </div>
+                                <div class="col-12 col-lg-9">
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <h4 class="mb-1">${product.name}</h4>
+                                        <small class="text-muted">${product.currency} ${product.cost}</small>
+                                    </div>
+                                    <p class="mb-1">${product.description}</p>
+                                    <p class="mb-1"><small>Cantidad vendidos: ${product.soldCount}</small></p>
                                 </div>
                             </div>
                         </div>
