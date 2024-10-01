02ac26b4e9bc89cdb626ec5b263446663d5d50bb
Brian <briancamilaa@gmail.com>
2024-10-01 11:27:24 -0300

    Solución de las fechas
    aa/mm/dd -> dd/mm/aa

--- js/product-info.js
+++ js/product-info.js
@@ -163,7 +163,7 @@ function formatDate(date) {
     let minutes = date.getMinutes().toString().padStart(2, '0');
     let seconds = date.getSeconds().toString().padStart(2, '0');
     
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
 
 function showErrorMessage() {
