a32264327d5dc3d3fbd7da5ccc3885a6cfd7e156
Yamila-e <yamila_escandon@hotmail.com>
2024-10-01 11:20:39 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
+    checkUserLogin();
     let productID = localStorage.getItem("productID");
 
     fetch(`https://japceibal.github.io/emercado-api/products/${productID}.json`)
-        .then(response => response.json())
-        .then(data => {
-            showProductInfo(data);
-        })
-        .catch(error => {
-            console.error("Error en fetch:", error);
-            showErrorMessage();
-        });
+    .then(response => response.json())
+    .then(data => {
+        product = data;
+        showProductInfo(product);
+        showRelatedProducts(product.relatedProducts);
+        return fetch(`https://japceibal.github.io/emercado-api/products_comments/${productID}.json`);
+    })
+    .then(response => response.json())
+    .then(data => {
+        comments = data;
+        showComments(comments);
+    })
+    .catch(error => {
+        console.error("Error en fetch:", error);
+        showErrorMessage();
+    });
 });
-
 function showProductInfo(product) {
     let productInfoContainer = document.getElementById("product-info-container");
     let productInfoHTML = `
@@ -48,6 +56,31 @@ function showProductInfo(product) {
         </div>
     `;
     productInfoContainer.innerHTML = productInfoHTML;
+}
+
+function showRelatedProducts(relatedProducts) {
+    let relatedProductsContainer = document.getElementById("related-products-container");
+    let relatedProductsHTML = `
+        <h4>Productos relacionados</h4>
+        <div class="row">
+            ${relatedProducts.map(product => `
+                <div class="col-md-3">
+                    <div class="card mb-3" onclick="setProductID(${product.id})">
+                        <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.name}</h5>
+                        </div>
+                    </div>
+                </div>
+            `).join('')}
+        </div>
+    `;
+    relatedProductsContainer.innerHTML = relatedProductsHTML;
+}
+
+function setProductID(id) {
+    localStorage.setItem("productID", id);
+    window.location.reload();
 }
 
 function showErrorMessage() {
